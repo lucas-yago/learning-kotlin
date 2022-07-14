203b58ceee96d3da21f13f2f78c6f60b8a78debc
Lucas Yago <lucasyago63@gmail.com>
2022-07-14 09:11:35 -0300

    Adiciona modulo orientaçao a objetos

---
+++ src/main/kotlin/OrientaçaoAObjetos/06_with.kt
@@ -0,0 +1,18 @@
+package OrientaçaoAObjetos
+
+fun main() {
+    val str = "Programação Kotlin"
+
+    str.uppercase()
+    str.lowercase()
+    val size = str.length
+
+    // Faz o uso da instância da classe para chamar diversos métodos
+    // Scope function
+    // Pode-se ler "com esse objeto, faça.."
+    with(str) {
+        uppercase()
+        lowercase()
+        val size = length
+    }
+}
